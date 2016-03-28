9e95471b6a6204254e5d8d897c33782b7ae9483b
Steven King <stevenking86@gmail.com>
2016-03-28 17:40:44 -0400

    render basic chart of a couple drive stats

--- app/assets/javascripts/drives_data_info.js
+++ app/assets/javascripts/drives_data_info.js
@@ -1,13 +1,17 @@
+// list of addresses for raw data.
+// addressList <- list(pullup_address = "http://stats.nba.com/js/data/sportvu/pullUpShootData.js",
+//     drives_address = "http://stats.nba.com/js/data/sportvu/drivesData.js", defense_address = "http://stats.nba.com/js/data/sportvu/defenseData.js",
+//     passing_address = "http://stats.nba.com/js/data/sportvu/passingData.js",
+//     touches_address = "http://stats.nba.com/js/data/sportvu/touchesData.js",
+//     speed_address = "http://stats.nba.com/js/data/sportvu/speedData.js", rebounding_address = "http://stats.nba.com/js/data/sportvu/reboundingData.js",
+//     catchshoot_address = "http://stats.nba.com/js/data/sportvu/catchShootData.js",
+//     shooting_address = "http://stats.nba.com/js/data/sportvu/shootingData.js")
+
+//DRIVES:
 // what to get at : drivesData.resultSets[0].rowSet
-
 // console log each player's drive stats: drivesData.resultSets[0].rowSet.forEach(function(entry) {console.log(entry)})
-
 // headers: drivesData.resultSets[0].headers
-
-// ["202391", "Jeremy Lin", "Jeremy", "Lin", "CHA", 70, 26, 6.7, 4, 7.4, 0.495, 7.4, 281, 472]
-
-// ["PLAYER_ID", "PLAYER", "FIRST_NAME", "LAST_NAME", "TEAM_ABBREVIATION", "GP", "MIN", "DVS", "DPP", "DTP", "FG_PCT", "PTS_48", "DPP_TOT", "DVS_TOT"]
-
+//ATTRIBUTES:
 // player id = 202391
 // player = jeremy Lin
 // first name = jeremy
@@ -17,7 +21,7 @@
 // min = minutes per game = 26
 // dvs = drives per game = 6.7
 // dpp = drive points per game = 4
-// dtp = ? = 7.4
+// dtp = % of team's personal fouls? = 7.4
 // fg_pct = fg % on drives I assume =  .495
 // pts_48 = drives from points per 48 minutes = 7.4
 // dpp_tot = drive points total = 281
@@ -42,8 +46,13 @@ function drive_player(entry) {
   this.drives_per_game = entry[7]
   this.points_from_drives_per_game = entry[8]
   this.dtp = entry[9]
-  this.fg_pct = entry[10]
-  this.pts_48 = entry[11]
+  this.fg_pct_on_drives = entry[10]
+  this.pts_48_on_drives = entry[11]
   this.points_from_drives_total = entry[12]
   this.total_drives = entry[13]
 }
+
+//var players = playerDrivesParser(drivesData.resultSets[0].rowSet)
+
+//driver code to sort players by drives per game:
+// var sorted_playas = playerz.sort(function(a,b){ return parseFloat(b.drives_per_game) - parseFloat(a.drives_per_game)})
---
+++ app/assets/javascripts/runner.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var players = playerDrivesParser(drivesData.resultSets[0].rowSet)
+  $(function () {
+      $('#container').highcharts({
+          chart: {
+              type: 'bar'
+          },
+          title: {
+              text: 'Drives'
+          },
+          xAxis: {
+              categories: ['Points from drives per game', 'Points from drives per 48 mins']
+          },
+          yAxis: {
+              title: {
+                  text: 'Points'
+              }
+          },
+          series: [{
+              name: players[0].name,
+              data: [players[0].points_from_drives_per_game, players[0].pts_48_on_drives]
+          }]
+      });
+  });
+
+})
