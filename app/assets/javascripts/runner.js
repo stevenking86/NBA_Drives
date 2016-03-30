156ff19951cee46a83bcbbc8e46762098a6bc571
Steven King <stevenking86@gmail.com>
2016-03-30 14:08:18 -0400

    Load other stats upon page load

--- app/assets/javascripts/runner.js
+++ app/assets/javascripts/runner.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var players = playerDrivesParser(drivesData.resultSets[0].rowSet)
+  updatePlayerAttributes(players[0])
   $(function () {
       $('#points_container').highcharts({
           chart: {
