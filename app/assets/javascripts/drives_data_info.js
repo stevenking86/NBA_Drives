14c87ad163c9c9263f0d0fe87f6f2b5e64ed18cb
Steven King <stevenking86@gmail.com>
2016-03-30 17:56:45 -0400

    Incorporate player images into design

--- app/assets/javascripts/drives_data_info.js
+++ app/assets/javascripts/drives_data_info.js
@@ -15,7 +15,7 @@ var playerFinder = function(player_name, players) {
   }
 
 function drive_player(entry) {
-  //this.player_id = entry[0]
+  this.player_id = entry[0]
   this.name = entry[1]
   this.first_name = entry[2]
   this.last_name = entry[3]
@@ -38,4 +38,5 @@ var updatePlayerAttributes = function(current_player){
   $("#fg_pct_drives").text("FG% on Drives: " + (current_player.fg_pct_on_drives * 100) + "%")
   $("#total_points_from_drives").text("Points from Drives Total: " + current_player.points_from_drives_total)
   // $("#total_drives").text(total_drives)
+  $("#player_image").html("<img src='http://stats.nba.com/media/players/230x185/" + current_player.player_id + ".png'>")
 }
